931d73d5a63627099ea7a33321680a1094a78c51
Loïc Herman <loic@loicherman.ch>
2023-11-14 14:56:41 +0100

    chore: add basic test to let CI pass

---
+++ src/test/java/heig/dai/pw02/MainTest.java
@@ -0,0 +1,13 @@
+package heig.dai.pw02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void helloJunit() {
+        assertEquals(2, 1 + 1);
+    }
+}
