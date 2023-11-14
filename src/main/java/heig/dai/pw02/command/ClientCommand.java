d5f6a35426053441d29e2bd0cbbe3f1de95b19d9
Loïc H <loic@loicherman.ch>
2023-11-14 14:58:05 +0100

    feat: add noop commands with options (#10)
    * feat: add noop commands with options
    
    closes #4
    
    * chore: add basic test to let CI pass

---
+++ src/main/java/heig/dai/pw02/command/ClientCommand.java
@@ -0,0 +1,48 @@
+package heig.dai.pw02.command;
+
+import java.util.concurrent.Callable;
+import lombok.extern.slf4j.Slf4j;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+/**
+ * Command to start a client to play chess
+ *
+ * @author Loïc Herman
+ * @author Massimo Steffani
+ */
+@Slf4j
+@Command(
+        name = "client",
+        description = "Start a client to play chess"
+)
+public class ClientCommand implements Callable<Integer> {
+
+    @Option(
+            names = {"-H", "--host"},
+            description = "server host IP address",
+            required = true
+    )
+    private String ipAddress;
+
+    @Option(
+            names = {"-p", "--port"},
+            description = "server port",
+            required = true
+    )
+    private int port;
+
+    @Option(
+            names = {"-g", "--gui"},
+            description = "enable the use of a GUI to play the game"
+    )
+    private boolean useGui;
+
+    @Override
+    public Integer call() throws Exception {
+        log.trace("Starting client");
+        log.trace("Connecting to {}:{}", ipAddress, port);
+        return 0;
+    }
+}
