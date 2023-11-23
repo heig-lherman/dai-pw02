c6f551deee9bd51d04ddd932a1f48fa0d0a9e54a
Massimo Stefani <massimo.cruz25@gmail.com>
2023-11-23 13:49:42 +0100

    CPPMessages not needed

--- src/main/java/heig/dai/pw02/ccp/CCPMessage.java
+++ src/main/java/heig/dai/pw02/ccp/CCPMessage.java
@@ -3,12 +3,6 @@ package heig.dai.pw02.ccp;
 import java.util.List;
 
 public enum CCPMessage {
-    COLOR, MOVE, PROMOTION, ERROR;
-
-    List<CCPEntity> entities;
-
-    CCPMessage(CCPEntity... entities) {
-        this.entities = List.of(entities);
-    }
+    COLOR, MOVE, PROMOTION, ERROR
 
 }
