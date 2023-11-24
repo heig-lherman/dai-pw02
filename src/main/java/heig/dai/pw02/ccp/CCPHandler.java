66862d1f81ab18b12daf8155e8a9731c2a054306
Massimo Stefani <massimo.cruz25@gmail.com>
2023-11-24 04:19:55 +0100

    protocol messages definition

---
+++ src/main/java/heig/dai/pw02/ccp/CCPHandler.java
@@ -0,0 +1,59 @@
+package heig.dai.pw02.ccp;
+
+import heig.dai.pw02.model.Message;
+import heig.dai.pw02.socket.SocketManager;
+import heig.poo.chess.engine.piece.ChessPiece;
+
+import java.net.Socket;
+import java.util.Stack;
+
+public abstract class CCPHandler {
+    private final SocketManager socketManager;
+    private final Stack<Message> messageStack = new Stack<>();
+
+    public CCPHandler(Socket socketManager) {
+        this.socketManager = new SocketManager(socketManager);
+    }
+
+    public void addToStack(Message message) {
+        messageStack.add(message);
+    }
+
+    public void sendStack() {
+        while (!messageStack.isEmpty()) {
+            sendMessage(messageStack.pop());
+        }
+    }
+
+    public void sendMove(int fromX, int fromY, int toX, int toY) {
+        sendMessage(new Message(CCPMessage.MOVE, fromX + " " + fromY + " " + toX + " " + toY));
+    }
+
+    public Message receiveMove() {
+        Message message = receiveMessage();
+        return message.type().equals(CCPMessage.MOVE) ? message : null;
+    }
+
+    public void addPromotionToStack(ChessPiece piece) {
+        addToStack(new Message(CCPMessage.PROMOTION,
+                piece.getPieceType().toString() + " " + piece.getX() + " " + piece.getY()));
+    }
+
+    public void addMoveToStack(int fromX, int fromY, int toX, int toY) {
+        messageStack.add(new Message(CCPMessage.MOVE, fromX + " " + fromY + " " + toX + " " + toY));
+    }
+
+    public Message receivePromotion() {
+        Message message = receiveMessage();
+        return message.type().equals(CCPMessage.PROMOTION) ? message : null;
+    }
+
+    public void sendMessage(Message message) {
+        socketManager.send(message);
+    }
+
+    public Message receiveMessage() {
+        return socketManager.read();
+    }
+
+}
--- src/main/java/heig/dai/pw02/ccp/CCPMessage.java
+++ src/main/java/heig/dai/pw02/ccp/CCPMessage.java
@@ -1,8 +1,5 @@
 package heig.dai.pw02.ccp;
-
-import java.util.List;
-
 public enum CCPMessage {
-    COLOR, MOVE, PROMOTION, ERROR
+    COLOR, MOVE, PROMOTION, REPLAY, ERROR
 
 }
