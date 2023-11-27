1b79fa38307749f39311f77ad4d034262cd0a867
massteff <42839432+massteff@users.noreply.github.com>
2023-11-27 11:21:46 +0100

    Update CCPMessage.java

--- src/main/java/heig/dai/pw02/ccp/CCPMessage.java
+++ src/main/java/heig/dai/pw02/ccp/CCPMessage.java
@@ -1,6 +1,6 @@
 package heig.dai.pw02.ccp;
 public enum CCPMessage {
-    COLOR(1), MOVE(4), PROMOTION(1), REPLAY(1), ERROR(1);
+    COLOR(1), MOVE(4), PROMOTION(3), REPLAY(1), ERROR(1);
     private final int nbrArguments;
 
     CCPMessage(int nbrArguments) {
