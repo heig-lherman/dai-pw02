cb2aab8d347aa0d8c3911d03f9493c514983b31b
Loïc Herman <loic@loicherman.ch>
2023-11-27 23:05:45 +0100

    refactor: reformat and handle player disconnects
    closes #11, closes #12

--- src/main/java/heig/dai/pw02/ccp/CCPHandler.java
+++ src/main/java/heig/dai/pw02/ccp/CCPHandler.java
@@ -1,16 +1,23 @@
 package heig.dai.pw02.ccp;
 
-import heig.dai.pw02.model.Message;
 import heig.dai.pw02.socket.SocketManager;
 import heig.poo.chess.engine.piece.ChessPiece;
 import heig.poo.chess.engine.util.Assertions;
+import heig.poo.chess.engine.util.ChessString;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.Socket;
-import java.util.Stack;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public abstract class CCPHandler {
+
     private final SocketManager socketManager;
-    private final Stack<Message<String>> messageStack = new Stack<>();
+
+    // We need this to put promotions on hold until we send the move message
+    private final AtomicReference<Message> pendingPromotion = new AtomicReference<>();
 
     public CCPHandler(Socket socketManager) {
         this.socketManager = new SocketManager(socketManager);
@@ -18,64 +25,72 @@ public abstract class CCPHandler {
 
     public void disconnect() {
         try {
-            socketManager.closeSocket();
+            socketManager.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new UncheckedIOException(e);
         }
     }
 
-    public void sendStack() {
-        while (!messageStack.isEmpty()) {
-            sendMessage(messageStack.pop());
-        }
-    }
-
-    public Message<String> receiveMove() {
-        return receiveMessage(CCPMessage.MOVE);
-    }
-
-    public Message<String> receivePromotion() {
-        return receiveMessage(CCPMessage.PROMOTION);
-    }
-
-    public Message<String> receiveReplay() {
-        return receiveMessage(CCPMessage.REPLAY);
-    }
-
-    public void addMoveToStack(int fromX, int fromY, int toX, int toY) {
-        addToStack(Message.withParsedArgsFromIntToString(new Message<>(CCPMessage.MOVE, fromX, fromY, toX, toY)));
-    }
-
-    public void addPromotionToStack(ChessPiece piece) {
-        addToStack(Message.withParsedArgsFromIntToString(
-                new Message<>(CCPMessage.PROMOTION, piece.getPieceType().ordinal(), piece.getX(), piece.getY())));
-    }
-
-    public void addReplayToStack(String replay) {
-        Assertions.assertTrue(replay.equals("Yes") || replay.equals("No"), "Replay must be Yes or No");
-        addToStack(new Message<>(CCPMessage.REPLAY, replay));
-    }
-
-    protected void addToStack(Message<String> message) {
-        messageStack.add(message);
-    }
-
-    protected void sendMessage(Message<String> message) {
+    protected void sendMessage(Message message) {
+        log.trace("Sending message of type {}", message.getType());
         socketManager.send(message);
     }
 
-    protected Message<String> receiveMessage() {
-        return socketManager.read();
+    protected CompletableFuture<Message> awaitMessage(CCPMessage type) {
+        log.trace("Waiting for message of type {}", type);
+        return CompletableFuture.supplyAsync(() -> {
+            Message message = socketManager.read();
+            if (message == null) {
+                log.warn("A client got disconnected");
+                System.exit(2);
+            }
+
+            if (message.getType() == type) {
+                return message;
+            }
+
+            log.warn("Received message of type {} instead of {}", message.getType(), type);
+            return null;
+        });
     }
 
-    protected Message<String> receiveMessage(CCPMessage type) {
-        Message<String> message = socketManager.read();
-        if(message.getType().equals(type)) {
-            return message;
-        }else{
-            // Send error message, type is not the expected one
-            return null;
+    public final CompletableFuture<Message> awaitMove() {
+        return awaitMessage(CCPMessage.MOVE);
+    }
+
+    public final CompletableFuture<Message> awaitPromotion() {
+        return awaitMessage(CCPMessage.PROMOTION);
+    }
+
+    public final CompletableFuture<Message> awaitReplay() {
+        return awaitMessage(CCPMessage.REPLAY);
+    }
+
+    public final void sendMove(int fromX, int fromY, int toX, int toY) {
+        sendMessage(Message.of(CCPMessage.MOVE, fromX, fromY, toX, toY));
+        if (pendingPromotion.get() != null) {
+            sendMessage(pendingPromotion.get());
+            pendingPromotion.set(null);
         }
     }
 
+    /**
+     * Send a promotion to the other player, but only once the move has been sent.
+     * @param piece the piece to promote to
+     */
+    public final void sendPromotion(ChessPiece piece) {
+        pendingPromotion.set(Message.of(
+                CCPMessage.PROMOTION,
+                piece.getPieceType().ordinal(),
+                piece.getX(), piece.getY()
+        ));
+    }
+
+    public final void sendReplay(String replay) {
+        Assertions.assertTrue(
+                replay.equals(ChessString.YES) || replay.equals(ChessString.NO),
+                "Replay must be Yes or No"
+        );
+        sendMessage(Message.of(CCPMessage.REPLAY, replay));
+    }
 }
